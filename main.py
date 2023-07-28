1ead6aabaec2746cefcf7baa3f05956ab0f44bd9
Cory Neville <cneville@gmail.com>
2023-07-27 21:58:27 -0400

    better parallel

--- main.py
+++ main.py
@@ -11,17 +11,23 @@ from dotenv import load_dotenv
 
 script_dir = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
 
-if 'API_ID' not in os.environ or 'API_KEY' not in os.environ:
-    load_dotenv(os.path.join(script_dir, '.env'))
-
-API_ID = os.environ['API_ID']
-API_KEY = os.environ['API_KEY']
-
 def read_tickers(DIR):
     with open(DIR) as file:
         lines = file.readlines()
         ticker_list = [line.rstrip() for line in lines]
         return ticker_list
+
+def load_creds_from_file(filepath):
+    with open(filepath, 'r') as creds_file:
+        return [c.replace('\n','').replace('\r','').strip() for c in creds_file.readlines()]
+
+def load_default_creds():
+    load_dotenv(os.path.join(script_dir, '.env'))
+    return os.environ['API_ID'], os.environ['API_KEY']
+
+def filter_tickers(tickers, filters):
+    filters = [f.lower() for f in filters.split(',')]
+    return [ticker for ticker in tickers if ticker.lower()[0] in filters]
 
 # %%
 # NOTE: https://github.com/microsoft/vscode-jupyter/issues/1837
@@ -29,18 +35,31 @@ is_vscode = 'VSCODE_CWD' in os.environ.keys()
 if is_vscode: sys.argv = ['']
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    import argparse
+    import argparse, pyarrow
     parser = argparse.ArgumentParser()
     parser.add_argument('--no-content', action='store_true')
     parser.add_argument('--reverse', action='store_true')
     parser.add_argument('--shuffle', action='store_true')
     parser.add_argument('--start', default='2015-01-01')
-    parser.add_argument('--end', default=f'{datetime.datetime.now()-datetime.timedelta(days=1):%Y-%m-%d}')
+    parser.add_argument('--end', default=f'{pd.Timestamp.now().now().normalize():%Y-%m-%d}')
+    parser.add_argument('--no-update', action='store_true')
+    parser.add_argument('--tickers', default=None)
+    parser.add_argument('--creds', default=None)
     config = parser.parse_args()
 
+    if 'API_ID' in os.environ and 'API_KEY' in os.environ:
+        api_id, api_key = os.environ['API_ID'], os.environ['API_KEY']
+    elif config.creds is not None:
+        api_id, api_key = load_creds_from_file(config.creds)
+    else:
+        api_id, api_key = load_default_creds()
+
     script_dir = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
-    retriever = ANR(API_ID, API_KEY, include_content=not config.no_content)
+    retriever = ANR(api_id, api_key, include_content=not config.no_content)
     tickers = read_tickers(os.path.join(script_dir, 'tickers.txt'))
+
+    if config.tickers is not None:
+        tickers = filter_tickers(tickers, config.tickers)
 
     if config.reverse:
         tickers = tickers[::-1]
@@ -55,11 +74,15 @@ if __name__ == '__main__':
             filepath = os.path.join(script_dir, 'data', f"{ticker}.parquet")
             file_exists = os.path.isfile(filepath)
             start = config.start
+            
             oldnews = pd.DataFrame()
             if file_exists:
-                oldnews = pd.read_parquet(filepath)
-                if not oldnews.empty:
-                    start = f'{oldnews.index.max() + pd.Timedelta(days=1):%Y-%m-%d}'
+                try:
+                    oldnews = pd.read_parquet(filepath)
+                except pyarrow.lib.ArrowInvalid:
+                    pass
+                if not oldnews.empty and not config.no_update:
+                    start = f'{oldnews.index.max().normalize():%Y-%m-%d}'
 
             news = retriever.get_news(ticker, start, config.end)
             
